b62babd3ac89b1617d0e7f35aa8202909093e382
farahdinazul <farahdinazul@gmail.com>
2026-07-02 22:54:37 +0800

    Changes to Exercise 04

--- py/Exercise_04.py
+++ py/Exercise_04.py
@@ -1,5 +1,5 @@
 weight = float(input("Enter your weight: "))
-height = float(input("Enter your height: "))
+height = float(input("Enter your height in meters: "))
 bmi = weight / (height ** 2)
 
 print("Your bmi is:", round(bmi,2))
